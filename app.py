abcca119bce49b5fbc81422649d96a00f03c5864
kinada-amd <k_inada@aimedia.co.jp>
2026-04-29 05:53:00 +0000

    style

--- app.py
+++ app.py
@@ -25,8 +25,10 @@ st.markdown("""
     div[data-baseweb="select"] > div { background-color: #FFFFFF !important; border: 1px solid #D5D9D9 !important; }
     div[data-testid="stMetricValue"] { color: #131921 !important; font-weight: 800 !important; letter-spacing: -0.03em !important; }
     h1, h2, h3 { color: #131921 !important; font-weight: 800 !important; }
-    .st-emotion-cache-zy6yx3 {padding-top: 3rem;padding-bottom: 3rem;}
+    .st-emotion-cache-zy6yx3 {padding-top: 2rem;padding-bottom: 3rem;}
     .st-emotion-cache-scp8yw {display: none!important;}
+    .st-emotion-cache-qmp9ai {visibility: visible !important;}
+    .st-emotion-cache-10p9htt {margin-bottom: 0 !important;}
     </style>
     """, unsafe_allow_html=True)
 
